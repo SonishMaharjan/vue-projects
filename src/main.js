97b016675352dcb19f81c05e26661fd792a30f4e
sonish <sonishmzn95@gmail.com>
2020-10-09 19:08:20 +0545

    bootstrap integrated

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+//styles
+import "@/assets/sass/index.sass";
+
+//dependencies
+import { BootstrapVue } from "bootstrap-vue";
+
+Vue.use(BootstrapVue);
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
